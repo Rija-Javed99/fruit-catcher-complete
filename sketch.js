642bd68490cd0672c15f09832a714f991a772964
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-08-23 16:39:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,25 +26,12 @@ function preload(){
 }
 function setup() {
   createCanvas(1000, 600);
-  database = firebase.database();
-  game = new Game();
-  game.getState();
-  game.start();
+  
   
 }
 
 function draw() {
   background(back_img);
   
-   if (playerCount === 2) {
-     game.update(1);
-   }
-   if (gameState === 1) {
-     clear(); 
-     game.play();
-   }
-   if (gameState === 2) {
-    
-     game.end();
-   }
+  
 }
